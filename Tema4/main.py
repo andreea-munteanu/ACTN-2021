5ca69ab5055ec323821af5fc1070119b7a4be61d
andreea-munteanu <57788684+andreea-munteanu@users.noreply.github.com>
2021-05-17 10:34:04 +0300

    clueless momentan

--- Tema4/main.py
+++ Tema4/main.py
@@ -2,20 +2,44 @@ import math
 import random
 import sympy
 from sympy import sieve
+import numpy as np
 
 
-def get_prime_divisors(p):
+"""
+1. Generate α, a primitive root modulo a prime p, where p is an odd prime, using one of the algorithms discussed in 
+class (assume that the prime factorization of p-1 is known in advance - the simplest choice will be p = 2q +1, 
+where p and q are odd primes). (2p)
+
+2. For p and α generated as above and an arbitrary β, compute logarithm modulo p to the base α of β, using one of 
+the algorithms discussed in class (Skanks or Pollard). Use moderate-sized primes (e.g., p is on 32 bits). (4p)
+
+3. Implement the Silver-Pohlig-Hellman algorithm for computing discrete logarithms modulo a large prime p 
+(e.g., p is on 1024 bits). Assume that p - 1 has only small prime divisors and that its prime factorization 
+is known in advance. (4p)
+"""
+
+
+def get_prime_divisors(p):  # works; checked
     """
     Method for obtaining the prime divisors of a number p.
 
     :param p:
-    :return:
+    :return: p = (p1 ** e1) * (p2 **e2) * ...
     """
     divs = []
     for prime_div in sieve.primerange(2, p):
-        if (p - 1) % prime_div == 0:
-            divs.append(prime_div)
+        if p % prime_div == 0:
+            copy = p
+            power = 0
+            while copy % prime_div == 0:
+                power += 1
+                copy /= prime_div
+            pair = (prime_div, power)
+            divs.append(pair)
     return divs
+
+
+# print(get_prime_divisors(100))
 
 
 def generate_alpha(p):
@@ -59,6 +83,57 @@ def generate_alpha2(p):  # works; checked
     lambda_symbol = random.randint(2, p - 1)
     alpha = (-1) * (lambda_symbol ** 2) % p
     return alpha
+
+
+def pollard_rho(alpha, n, beta):
+    """
+
+    :param alpha: generator of cyclic group G
+    :param n: order of cyclic group G
+    :param beta: element belonging to G
+    :return: discrete logarithm x = log_alpha(beta)
+    """
+    x = a = b = np.zeros(n)
+    x[0] = 1
+    a[0] = b[0] = 0
+    for i in range(1, n):
+        pass
+
+
+def pohlig_hellman(alpha, n, beta):
+    """
+
+    :param alpha: generator of cyclic group G
+    :param n: order of cyclic group G
+    :param beta: element belonging to G
+    :return: discrete logarithm x = log_alpha(beta)
+    """
+    prime_divs = get_prime_divisors(n)
+    r = len(prime_divs)
+    for pair in prime_divs:
+        q = pair[0]
+        e = pair[1]
+        lambdaa = 1
+        l = 0  # l(-1)
+        x_i = l
+        power = 1
+        alpha_bar = pow(alpha, n/q)
+
+        for j in range(e-1):
+            lambdaa *= pow(alpha, l * (q ** (j - 1)) )
+            beta_bar = pow(beta * pow(lambdaa, -1), n / (q ** (j + 1)))
+
+            # aici ar trebui 3.56 (baby-step giant fml):
+            l = math.log(beta_bar, alpha_bar)  # log(x, base)
+            x_i += l * pow(q, power)
+            power += 1
+
+    # computing x, 0 <= x <= n-1, s.t. x = x_i % pow(p_i, e_i) for i = 1,...,r
+    x = 0
+    for i in range(r):
+        # computing x using some Gauss algorithm I can't find
+        pass
+    return x
 
 
 if __name__ == '__main__':
